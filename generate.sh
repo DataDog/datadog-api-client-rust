f50565fc23a1426c40373411856979497f3b1c3d
Kevin Zou <kevin.zou@datadoghq.com>
2024-02-02 11:05:16 -0500

    fix license script

--- generate.sh
+++ generate.sh
@@ -27,3 +27,4 @@ rm -rf src/* examples/*
 pre_commit_wrapper generator
 pre_commit_wrapper format
 pre_commit_wrapper lint
+pre_commit_wrapper license-check
---
+++ scripts/license-check.sh
@@ -0,0 +1,3 @@
+cargo install --quiet dd-rust-license-tool
+dd-rust-license-tool check
+exit 0
