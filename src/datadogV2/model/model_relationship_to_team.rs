9aa4c1127f8ef515a0619299873b81ae244a83f5
ci.datadog-api-spec <packages@datadoghq.com>
2024-04-08 20:27:52 +0000

    Regenerate client from commit 037e376a of spec repo

---
+++ src/datadogV2/model/model_relationship_to_team.rs
@@ -0,0 +1,82 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::skip_serializing_none;
+use std::fmt::{self, Formatter};
+
+/// Relationship to team.
+#[non_exhaustive]
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub struct RelationshipToTeam {
+    /// Relationship to Team object.
+    #[serde(rename = "data")]
+    pub data: Option<crate::datadogV2::model::RelationshipToTeamData>,
+    #[serde(skip)]
+    #[serde(default)]
+    pub(crate) _unparsed: bool,
+}
+
+impl RelationshipToTeam {
+    pub fn new() -> RelationshipToTeam {
+        RelationshipToTeam {
+            data: None,
+            _unparsed: false,
+        }
+    }
+
+    pub fn data(mut self, value: crate::datadogV2::model::RelationshipToTeamData) -> Self {
+        self.data = Some(value);
+        self
+    }
+}
+
+impl Default for RelationshipToTeam {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'de> Deserialize<'de> for RelationshipToTeam {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct RelationshipToTeamVisitor;
+        impl<'a> Visitor<'a> for RelationshipToTeamVisitor {
+            type Value = RelationshipToTeam;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("a mapping")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'a>,
+            {
+                let mut data: Option<crate::datadogV2::model::RelationshipToTeamData> = None;
+                let mut _unparsed = false;
+
+                while let Some((k, v)) = map.next_entry::<String, serde_json::Value>()? {
+                    match k.as_str() {
+                        "data" => {
+                            if v.is_null() {
+                                continue;
+                            }
+                            data = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        &_ => {}
+                    }
+                }
+
+                let content = RelationshipToTeam { data, _unparsed };
+
+                Ok(content)
+            }
+        }
+
+        deserializer.deserialize_any(RelationshipToTeamVisitor)
+    }
+}
