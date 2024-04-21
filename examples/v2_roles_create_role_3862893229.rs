a9597a581afe5eaef6a2a7b7a6986459a8e10249
ci.datadog-api-spec <packages@datadoghq.com>
2024-04-21 03:10:33 +0000

    Regenerate client from commit 71b7fbd9 of spec repo

--- examples/v2_roles_create_role_3862893229.rs
+++ examples/v2_roles_create_role_3862893229.rs
@@ -1,10 +1,9 @@
-// Create role returns "OK" response
+// Create role with a permission returns "OK" response
 use datadog_api_client::datadog;
 use datadog_api_client::datadogV2::api_roles::RolesAPI;
 use datadog_api_client::datadogV2::model::PermissionsType;
 use datadog_api_client::datadogV2::model::RelationshipToPermissionData;
 use datadog_api_client::datadogV2::model::RelationshipToPermissions;
-use datadog_api_client::datadogV2::model::RelationshipToUsers;
 use datadog_api_client::datadogV2::model::RoleCreateAttributes;
 use datadog_api_client::datadogV2::model::RoleCreateData;
 use datadog_api_client::datadogV2::model::RoleCreateRequest;
@@ -14,14 +13,14 @@ use datadog_api_client::datadogV2::model::RolesType;
 #[tokio::main]
 async fn main() {
     let body = RoleCreateRequest::new(
-        RoleCreateData::new(RoleCreateAttributes::new("developers".to_string()))
-            .relationships(
-                RoleRelationships::new()
-                    .permissions(RelationshipToPermissions::new().data(vec![
-                        RelationshipToPermissionData::new().type_(PermissionsType::PERMISSIONS),
-                    ]))
-                    .users(RelationshipToUsers::new(vec![])),
-            )
+        RoleCreateData::new(RoleCreateAttributes::new("Example-Role".to_string()))
+            .relationships(RoleRelationships::new().permissions(
+                RelationshipToPermissions::new().data(vec![
+                                RelationshipToPermissionData::new()
+                                    .id("d90f6831-d3d8-11e9-a77a-4fd230ddbc6a".to_string())
+                                    .type_(PermissionsType::PERMISSIONS)
+                            ]),
+            ))
             .type_(RolesType::ROLES),
     );
     let configuration = datadog::Configuration::new();
