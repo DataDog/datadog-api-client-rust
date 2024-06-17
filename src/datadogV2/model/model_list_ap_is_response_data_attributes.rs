c5a42b3ac79c362392f2643ade21e72e1e08a99c
ci.datadog-api-spec <packages@datadoghq.com>
2024-06-17 09:26:29 +0000

    Regenerate client from commit 743cf92b of spec repo

---
+++ src/datadogV2/model/model_list_ap_is_response_data_attributes.rs
@@ -0,0 +1,82 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::skip_serializing_none;
+use std::fmt::{self, Formatter};
+
+/// Attributes for `ListAPIsResponseData`.
+#[non_exhaustive]
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub struct ListAPIsResponseDataAttributes {
+    /// API name.
+    #[serde(rename = "name")]
+    pub name: Option<String>,
+    #[serde(skip)]
+    #[serde(default)]
+    pub(crate) _unparsed: bool,
+}
+
+impl ListAPIsResponseDataAttributes {
+    pub fn new() -> ListAPIsResponseDataAttributes {
+        ListAPIsResponseDataAttributes {
+            name: None,
+            _unparsed: false,
+        }
+    }
+
+    pub fn name(mut self, value: String) -> Self {
+        self.name = Some(value);
+        self
+    }
+}
+
+impl Default for ListAPIsResponseDataAttributes {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'de> Deserialize<'de> for ListAPIsResponseDataAttributes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ListAPIsResponseDataAttributesVisitor;
+        impl<'a> Visitor<'a> for ListAPIsResponseDataAttributesVisitor {
+            type Value = ListAPIsResponseDataAttributes;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("a mapping")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'a>,
+            {
+                let mut name: Option<String> = None;
+                let mut _unparsed = false;
+
+                while let Some((k, v)) = map.next_entry::<String, serde_json::Value>()? {
+                    match k.as_str() {
+                        "name" => {
+                            if v.is_null() {
+                                continue;
+                            }
+                            name = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        &_ => {}
+                    }
+                }
+
+                let content = ListAPIsResponseDataAttributes { name, _unparsed };
+
+                Ok(content)
+            }
+        }
+
+        deserializer.deserialize_any(ListAPIsResponseDataAttributesVisitor)
+    }
+}
