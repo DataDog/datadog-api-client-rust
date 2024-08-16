d2141f9cf38cc3ed011bf048c5aca3518d4165ff
ci.datadog-api-spec <packages@datadoghq.com>
2024-08-16 13:45:33 +0000

    Regenerate client from commit 1b56c3ba of spec repo

--- examples/v1_synthetics_CreateGlobalVariable.rs
+++ examples/v1_synthetics_CreateGlobalVariable.rs
@@ -1,23 +1,20 @@
 // Create a global variable returns "OK" response
 use datadog_api_client::datadog;
 use datadog_api_client::datadogV1::api_synthetics::SyntheticsAPI;
-use datadog_api_client::datadogV1::model::SyntheticsGlobalVariable;
 use datadog_api_client::datadogV1::model::SyntheticsGlobalVariableAttributes;
 use datadog_api_client::datadogV1::model::SyntheticsGlobalVariableParseTestOptions;
 use datadog_api_client::datadogV1::model::SyntheticsGlobalVariableParseTestOptionsType;
 use datadog_api_client::datadogV1::model::SyntheticsGlobalVariableParserType;
+use datadog_api_client::datadogV1::model::SyntheticsGlobalVariableRequest;
 use datadog_api_client::datadogV1::model::SyntheticsGlobalVariableValue;
 use datadog_api_client::datadogV1::model::SyntheticsVariableParser;
 
 #[tokio::main]
 async fn main() {
-    let body = SyntheticsGlobalVariable::new(
+    let body = SyntheticsGlobalVariableRequest::new(
         "Example description".to_string(),
         "MY_VARIABLE".to_string(),
         vec!["team:front".to_string(), "test:workflow-1".to_string()],
-        SyntheticsGlobalVariableValue::new()
-            .secure(true)
-            .value("value".to_string()),
     )
     .attributes(
         SyntheticsGlobalVariableAttributes::new()
@@ -34,7 +31,12 @@ async fn main() {
                 .value(".*".to_string()),
         ),
     )
-    .parse_test_public_id("abc-def-123".to_string());
+    .parse_test_public_id("abc-def-123".to_string())
+    .value(
+        SyntheticsGlobalVariableValue::new()
+            .secure(true)
+            .value("value".to_string()),
+    );
     let configuration = datadog::Configuration::new();
     let api = SyntheticsAPI::with_config(configuration);
     let resp = api.create_global_variable(body).await;
