d6572e43f11ab7b0750b609eee8acc3bf48a7270
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-15 14:47:56 +0000

    Regenerate client from commit 85278ed5 of spec repo

--- src/datadogV1/model/model_synthetics_ci_test_body.rs
+++ src/datadogV1/model/model_synthetics_ci_test_body.rs
@@ -11,7 +11,7 @@ use std::fmt::{self, Formatter};
 #[skip_serializing_none]
 #[derive(Clone, Debug, PartialEq, Serialize)]
 pub struct SyntheticsCITestBody {
-    /// Individual synthetics test.
+    /// List of individual synthetic tests.
     #[serde(rename = "tests")]
     pub tests: Option<Vec<crate::datadogV1::model::SyntheticsCITest>>,
     #[serde(flatten)]
