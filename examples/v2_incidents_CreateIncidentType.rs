981005588647e1dbb4dd82562fc7bc923539b899
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-24 17:10:51 +0000

    Regenerate client from commit e6fd239f of spec repo

---
+++ examples/v2_incidents_CreateIncidentType.rs
@@ -0,0 +1,26 @@
+// Create an incident type returns "CREATED" response
+use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_incidents::IncidentsAPI;
+use datadog_api_client::datadogV2::model::IncidentTypeAttributes;
+use datadog_api_client::datadogV2::model::IncidentTypeCreateData;
+use datadog_api_client::datadogV2::model::IncidentTypeCreateRequest;
+use datadog_api_client::datadogV2::model::IncidentTypeType;
+
+#[tokio::main]
+async fn main() {
+    let body = IncidentTypeCreateRequest::new(IncidentTypeCreateData::new(
+        IncidentTypeAttributes::new("8d4bbb66dccf2456".to_string())
+            .description("8d4bbb66dccf2456".to_string())
+            .is_default(true),
+        IncidentTypeType::INCIDENT_TYPES,
+    ));
+    let mut configuration = datadog::Configuration::new();
+    configuration.set_unstable_operation_enabled("v2.CreateIncidentType", true);
+    let api = IncidentsAPI::with_config(configuration);
+    let resp = api.create_incident_type(body).await;
+    if let Ok(value) = resp {
+        println!("{:#?}", value);
+    } else {
+        println!("{:#?}", resp.unwrap_err());
+    }
+}
---
+++ examples/v2_incidents_UpdateIncidentType.rs
@@ -0,0 +1,29 @@
+// Update an incident type returns "OK" response
+use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_incidents::IncidentsAPI;
+use datadog_api_client::datadogV2::model::IncidentTypePatchData;
+use datadog_api_client::datadogV2::model::IncidentTypePatchRequest;
+use datadog_api_client::datadogV2::model::IncidentTypeType;
+use datadog_api_client::datadogV2::model::IncidentTypeUpdateAttributes;
+
+#[tokio::main]
+async fn main() {
+    // there is a valid "incident_type" in the system
+    let incident_type_data_id = std::env::var("INCIDENT_TYPE_DATA_ID").unwrap();
+    let body = IncidentTypePatchRequest::new(IncidentTypePatchData::new(
+        IncidentTypeUpdateAttributes::new().name("Security Incident-updated".to_string()),
+        incident_type_data_id.clone(),
+        IncidentTypeType::INCIDENT_TYPES,
+    ));
+    let mut configuration = datadog::Configuration::new();
+    configuration.set_unstable_operation_enabled("v2.UpdateIncidentType", true);
+    let api = IncidentsAPI::with_config(configuration);
+    let resp = api
+        .update_incident_type(incident_type_data_id.clone(), body)
+        .await;
+    if let Ok(value) = resp {
+        println!("{:#?}", value);
+    } else {
+        println!("{:#?}", resp.unwrap_err());
+    }
+}
