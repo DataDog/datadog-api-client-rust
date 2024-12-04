b23792bbc6d0022cf7a16c381d530f1c0c160c78
ci.datadog-api-spec <packages@datadoghq.com>
2024-12-04 22:32:27 +0000

    Regenerate client from commit 142985c7 of spec repo

--- src/datadogV2/api/api_cloud_cost_management.rs
+++ src/datadogV2/api/api_cloud_cost_management.rs
@@ -50,14 +50,6 @@ pub enum DeleteCustomCostsFileError {
     UnknownValue(serde_json::Value),
 }
 
-/// GetCloudCostActivityError is a struct for typed errors of method [`CloudCostManagementAPI::get_cloud_cost_activity`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum GetCloudCostActivityError {
-    APIErrorResponse(crate::datadogV2::model::APIErrorResponse),
-    UnknownValue(serde_json::Value),
-}
-
 /// GetCustomCostsFileError is a struct for typed errors of method [`CloudCostManagementAPI::get_custom_costs_file`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -747,112 +739,6 @@ impl CloudCostManagementAPI {
             })
         } else {
             let local_entity: Option<DeleteCustomCostsFileError> =
-                serde_json::from_str(&local_content).ok();
-            let local_error = datadog::ResponseContent {
-                status: local_status,
-                content: local_content,
-                entity: local_entity,
-            };
-            Err(datadog::Error::ResponseError(local_error))
-        }
-    }
-
-    /// Get the Cloud Cost Management activity.
-    pub async fn get_cloud_cost_activity(
-        &self,
-    ) -> Result<
-        crate::datadogV2::model::CloudCostActivityResponse,
-        datadog::Error<GetCloudCostActivityError>,
-    > {
-        match self.get_cloud_cost_activity_with_http_info().await {
-            Ok(response_content) => {
-                if let Some(e) = response_content.entity {
-                    Ok(e)
-                } else {
-                    Err(datadog::Error::Serde(serde::de::Error::custom(
-                        "response content was None",
-                    )))
-                }
-            }
-            Err(err) => Err(err),
-        }
-    }
-
-    /// Get the Cloud Cost Management activity.
-    pub async fn get_cloud_cost_activity_with_http_info(
-        &self,
-    ) -> Result<
-        datadog::ResponseContent<crate::datadogV2::model::CloudCostActivityResponse>,
-        datadog::Error<GetCloudCostActivityError>,
-    > {
-        let local_configuration = &self.config;
-        let operation_id = "v2.get_cloud_cost_activity";
-
-        let local_client = &self.client;
-
-        let local_uri_str = format!(
-            "{}/api/v2/cost/enabled",
-            local_configuration.get_operation_host(operation_id)
-        );
-        let mut local_req_builder =
-            local_client.request(reqwest::Method::GET, local_uri_str.as_str());
-
-        // build headers
-        let mut headers = HeaderMap::new();
-        headers.insert("Accept", HeaderValue::from_static("application/json"));
-
-        // build user agent
-        match HeaderValue::from_str(local_configuration.user_agent.as_str()) {
-            Ok(user_agent) => headers.insert(reqwest::header::USER_AGENT, user_agent),
-            Err(e) => {
-                log::warn!("Failed to parse user agent header: {e}, falling back to default");
-                headers.insert(
-                    reqwest::header::USER_AGENT,
-                    HeaderValue::from_static(datadog::DEFAULT_USER_AGENT.as_str()),
-                )
-            }
-        };
-
-        // build auth
-        if let Some(local_key) = local_configuration.auth_keys.get("apiKeyAuth") {
-            headers.insert(
-                "DD-API-KEY",
-                HeaderValue::from_str(local_key.key.as_str())
-                    .expect("failed to parse DD-API-KEY header"),
-            );
-        };
-        if let Some(local_key) = local_configuration.auth_keys.get("appKeyAuth") {
-            headers.insert(
-                "DD-APPLICATION-KEY",
-                HeaderValue::from_str(local_key.key.as_str())
-                    .expect("failed to parse DD-APPLICATION-KEY header"),
-            );
-        };
-
-        local_req_builder = local_req_builder.headers(headers);
-        let local_req = local_req_builder.build()?;
-        log::debug!("request content: {:?}", local_req.body());
-        let local_resp = local_client.execute(local_req).await?;
-
-        let local_status = local_resp.status();
-        let local_content = local_resp.text().await?;
-        log::debug!("response content: {}", local_content);
-
-        if !local_status.is_client_error() && !local_status.is_server_error() {
-            match serde_json::from_str::<crate::datadogV2::model::CloudCostActivityResponse>(
-                &local_content,
-            ) {
-                Ok(e) => {
-                    return Ok(datadog::ResponseContent {
-                        status: local_status,
-                        content: local_content,
-                        entity: Some(e),
-                    })
-                }
-                Err(e) => return Err(datadog::Error::Serde(e)),
-            };
-        } else {
-            let local_entity: Option<GetCloudCostActivityError> =
                 serde_json::from_str(&local_content).ok();
             let local_error = datadog::ResponseContent {
                 status: local_status,
