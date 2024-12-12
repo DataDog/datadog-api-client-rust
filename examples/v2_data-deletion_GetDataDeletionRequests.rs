faec906bacd23e7220a0d15101fd8518b5575a9e
ci.datadog-api-spec <packages@datadoghq.com>
2024-12-12 08:49:34 +0000

    Regenerate client from commit 7163da4d of spec repo

--- examples/v2_data-deletion_GetDataDeletionRequests.rs
+++ examples/v2_data-deletion_GetDataDeletionRequests.rs
@@ -5,7 +5,8 @@ use datadog_api_client::datadogV2::api_data_deletion::GetDataDeletionRequestsOpt
 
 #[tokio::main]
 async fn main() {
-    let configuration = datadog::Configuration::new();
+    let mut configuration = datadog::Configuration::new();
+    configuration.set_unstable_operation_enabled("v2.GetDataDeletionRequests", true);
     let api = DataDeletionAPI::with_config(configuration);
     let resp = api
         .get_data_deletion_requests(GetDataDeletionRequestsOptionalParams::default())
