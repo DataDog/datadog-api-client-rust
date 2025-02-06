d5485632e7c6414cb0ad0af5781a890dedcb1e32
ci.datadog-api-spec <packages@datadoghq.com>
2025-02-06 15:05:29 +0000

    Regenerate client from commit 35bc11b5 of spec repo

--- examples/v2_dora-metrics_CreateDORADeployment.rs
+++ examples/v2_dora-metrics_CreateDORADeployment.rs
@@ -9,16 +9,12 @@ use datadog_api_client::datadogV2::model::DORAGitInfo;
 #[tokio::main]
 async fn main() {
     let body = DORADeploymentRequest::new(DORADeploymentRequestData::new(
-        DORADeploymentRequestAttributes::new(
-            1693491984000000000,
-            "shopist".to_string(),
-            1693491974000000000,
-        )
-        .git(DORAGitInfo::new(
-            "66adc9350f2cc9b250b69abddab733dd55e1a588".to_string(),
-            "https://github.com/organization/example-repository".to_string(),
-        ))
-        .version("v1.12.07".to_string()),
+        DORADeploymentRequestAttributes::new(1693491984000, "shopist".to_string(), 1693491974000)
+            .git(DORAGitInfo::new(
+                "66adc9350f2cc9b250b69abddab733dd55e1a588".to_string(),
+                "https://github.com/organization/example-repository".to_string(),
+            ))
+            .version("v1.12.07".to_string()),
     ));
     let mut configuration = datadog::Configuration::new();
     configuration.set_unstable_operation_enabled("v2.CreateDORADeployment", true);
