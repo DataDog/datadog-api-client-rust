15859c44061dfcf5c72388129fe505f9a7ba262b
ci.datadog-api-spec <packages@datadoghq.com>
2025-02-18 17:00:02 +0000

    Regenerate client from commit 66bd9a6a of spec repo

---
+++ examples/v2_application-security_DeleteApplicationSecurityExclusionFilter.rs
@@ -0,0 +1,17 @@
+// Delete an Application Security exclusion filter returns "OK" response
+use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_application_security::ApplicationSecurityAPI;
+
+#[tokio::main]
+async fn main() {
+    let configuration = datadog::Configuration::new();
+    let api = ApplicationSecurityAPI::with_config(configuration);
+    let resp = api
+        .delete_application_security_exclusion_filter("exclusion_filter_id".to_string())
+        .await;
+    if let Ok(value) = resp {
+        println!("{:#?}", value);
+    } else {
+        println!("{:#?}", resp.unwrap_err());
+    }
+}
