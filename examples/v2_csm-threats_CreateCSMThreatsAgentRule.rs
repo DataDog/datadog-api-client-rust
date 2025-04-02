654c2209ec7a493a9c5d66e23ca58eff0ddad2c0
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-02 13:47:20 +0000

    Regenerate client from commit 75e7a3b4 of spec repo

--- examples/v2_csm-threats_CreateCSMThreatsAgentRule.rs
+++ examples/v2_csm-threats_CreateCSMThreatsAgentRule.rs
@@ -8,6 +8,8 @@ use datadog_api_client::datadogV2::model::CloudWorkloadSecurityAgentRuleType;
 
 #[tokio::main]
 async fn main() {
+    // there is a valid "policy_rc" in the system
+    let policy_data_id = std::env::var("POLICY_DATA_ID").unwrap();
     let body = CloudWorkloadSecurityAgentRuleCreateRequest::new(
         CloudWorkloadSecurityAgentRuleCreateData::new(
             CloudWorkloadSecurityAgentRuleCreateAttributes::new(
@@ -16,7 +18,9 @@ async fn main() {
             )
             .description("My Agent rule".to_string())
             .enabled(true)
-            .filters(vec![r#"os == "linux""#.to_string()]),
+            .filters(vec![])
+            .policy_id(policy_data_id.clone())
+            .product_tags(vec![]),
             CloudWorkloadSecurityAgentRuleType::AGENT_RULE,
         ),
     );
