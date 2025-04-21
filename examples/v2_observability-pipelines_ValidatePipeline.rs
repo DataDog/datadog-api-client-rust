81cb38807194eb1a84e932414ed46879f0970710
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-21 13:05:20 +0000

    Regenerate client from commit 28eb0f35 of spec repo

---
+++ examples/v2_observability-pipelines_ValidatePipeline.rs
@@ -0,0 +1,73 @@
+// Validate an observability pipeline returns "OK" response
+use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_observability_pipelines::ObservabilityPipelinesAPI;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineConfig;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineConfigDestinationItem;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineConfigProcessorItem;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineConfigSourceItem;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineCreateRequest;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineCreateRequestData;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineDataAttributes;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineDatadogAgentSource;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineDatadogAgentSourceType;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineDatadogLogsDestination;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineDatadogLogsDestinationType;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineFilterProcessor;
+use datadog_api_client::datadogV2::model::ObservabilityPipelineFilterProcessorType;
+
+#[tokio::main]
+async fn main() {
+    let body =
+        ObservabilityPipelineCreateRequest::new(
+            ObservabilityPipelineCreateRequestData::new(
+                ObservabilityPipelineDataAttributes::new(
+                    ObservabilityPipelineConfig::new(
+                        vec![
+                            ObservabilityPipelineConfigDestinationItem::ObservabilityPipelineDatadogLogsDestination(
+                                Box::new(
+                                    ObservabilityPipelineDatadogLogsDestination::new(
+                                        "datadog-logs-destination".to_string(),
+                                        vec!["filter-processor".to_string()],
+                                        ObservabilityPipelineDatadogLogsDestinationType::DATADOG_LOGS,
+                                    ),
+                                ),
+                            )
+                        ],
+                        vec![
+                            ObservabilityPipelineConfigProcessorItem::ObservabilityPipelineFilterProcessor(
+                                Box::new(
+                                    ObservabilityPipelineFilterProcessor::new(
+                                        "filter-processor".to_string(),
+                                        "service:my-service".to_string(),
+                                        vec!["datadog-agent-source".to_string()],
+                                        ObservabilityPipelineFilterProcessorType::FILTER,
+                                    ),
+                                ),
+                            )
+                        ],
+                        vec![
+                            ObservabilityPipelineConfigSourceItem::ObservabilityPipelineDatadogAgentSource(
+                                Box::new(
+                                    ObservabilityPipelineDatadogAgentSource::new(
+                                        "datadog-agent-source".to_string(),
+                                        ObservabilityPipelineDatadogAgentSourceType::DATADOG_AGENT,
+                                    ),
+                                ),
+                            )
+                        ],
+                    ),
+                    "Main Observability Pipeline".to_string(),
+                ),
+                "pipelines".to_string(),
+            ),
+        );
+    let mut configuration = datadog::Configuration::new();
+    configuration.set_unstable_operation_enabled("v2.ValidatePipeline", true);
+    let api = ObservabilityPipelinesAPI::with_config(configuration);
+    let resp = api.validate_pipeline(body).await;
+    if let Ok(value) = resp {
+        println!("{:#?}", value);
+    } else {
+        println!("{:#?}", resp.unwrap_err());
+    }
+}
---
+++ src/datadogV2/model/model_validation_error_response.rs
@@ -0,0 +1,105 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::skip_serializing_none;
+use std::fmt::{self, Formatter};
+
+/// Response containing validation errors.
+#[non_exhaustive]
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub struct ValidationErrorResponse {
+    /// The `ValidationErrorResponse` `errors`.
+    #[serde(rename = "errors")]
+    pub errors: Option<Vec<crate::datadogV2::model::ValidationError>>,
+    #[serde(flatten)]
+    pub additional_properties: std::collections::BTreeMap<String, serde_json::Value>,
+    #[serde(skip)]
+    #[serde(default)]
+    pub(crate) _unparsed: bool,
+}
+
+impl ValidationErrorResponse {
+    pub fn new() -> ValidationErrorResponse {
+        ValidationErrorResponse {
+            errors: None,
+            additional_properties: std::collections::BTreeMap::new(),
+            _unparsed: false,
+        }
+    }
+
+    pub fn errors(mut self, value: Vec<crate::datadogV2::model::ValidationError>) -> Self {
+        self.errors = Some(value);
+        self
+    }
+
+    pub fn additional_properties(
+        mut self,
+        value: std::collections::BTreeMap<String, serde_json::Value>,
+    ) -> Self {
+        self.additional_properties = value;
+        self
+    }
+}
+
+impl Default for ValidationErrorResponse {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'de> Deserialize<'de> for ValidationErrorResponse {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ValidationErrorResponseVisitor;
+        impl<'a> Visitor<'a> for ValidationErrorResponseVisitor {
+            type Value = ValidationErrorResponse;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("a mapping")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'a>,
+            {
+                let mut errors: Option<Vec<crate::datadogV2::model::ValidationError>> = None;
+                let mut additional_properties: std::collections::BTreeMap<
+                    String,
+                    serde_json::Value,
+                > = std::collections::BTreeMap::new();
+                let mut _unparsed = false;
+
+                while let Some((k, v)) = map.next_entry::<String, serde_json::Value>()? {
+                    match k.as_str() {
+                        "errors" => {
+                            if v.is_null() {
+                                continue;
+                            }
+                            errors = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        &_ => {
+                            if let Ok(value) = serde_json::from_value(v.clone()) {
+                                additional_properties.insert(k, value);
+                            }
+                        }
+                    }
+                }
+
+                let content = ValidationErrorResponse {
+                    errors,
+                    additional_properties,
+                    _unparsed,
+                };
+
+                Ok(content)
+            }
+        }
+
+        deserializer.deserialize_any(ValidationErrorResponseVisitor)
+    }
+}
