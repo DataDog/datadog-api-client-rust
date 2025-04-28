5219d5ab79bcdc63d8539f6141f9cb90d82ef9e4
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-28 14:26:40 +0000

    Regenerate client from commit 696be8b0 of spec repo

--- examples/v2_csm-threats_UpdateCSMThreatsAgentRule.rs
+++ examples/v2_csm-threats_UpdateCSMThreatsAgentRule.rs
@@ -1,6 +1,7 @@
 // Update a CSM Threats Agent rule returns "OK" response
 use datadog_api_client::datadog;
 use datadog_api_client::datadogV2::api_csm_threats::CSMThreatsAPI;
+use datadog_api_client::datadogV2::api_csm_threats::UpdateCSMThreatsAgentRuleOptionalParams;
 use datadog_api_client::datadogV2::model::CloudWorkloadSecurityAgentRuleType;
 use datadog_api_client::datadogV2::model::CloudWorkloadSecurityAgentRuleUpdateAttributes;
 use datadog_api_client::datadogV2::model::CloudWorkloadSecurityAgentRuleUpdateData;
@@ -10,12 +11,17 @@ use datadog_api_client::datadogV2::model::CloudWorkloadSecurityAgentRuleUpdateRe
 async fn main() {
     // there is a valid "agent_rule_rc" in the system
     let agent_rule_data_id = std::env::var("AGENT_RULE_DATA_ID").unwrap();
+
+    // there is a valid "policy_rc" in the system
+    let policy_data_id = std::env::var("POLICY_DATA_ID").unwrap();
     let body = CloudWorkloadSecurityAgentRuleUpdateRequest::new(
         CloudWorkloadSecurityAgentRuleUpdateData::new(
             CloudWorkloadSecurityAgentRuleUpdateAttributes::new()
-                .description("Test Agent rule".to_string())
+                .description("My Agent rule".to_string())
                 .enabled(true)
-                .expression(r#"exec.file.name == "sh""#.to_string()),
+                .expression(r#"exec.file.name == "sh""#.to_string())
+                .policy_id(policy_data_id.clone())
+                .product_tags(vec![]),
             CloudWorkloadSecurityAgentRuleType::AGENT_RULE,
         )
         .id(agent_rule_data_id.clone()),
@@ -23,7 +29,11 @@ async fn main() {
     let configuration = datadog::Configuration::new();
     let api = CSMThreatsAPI::with_config(configuration);
     let resp = api
-        .update_csm_threats_agent_rule(agent_rule_data_id.clone(), body)
+        .update_csm_threats_agent_rule(
+            agent_rule_data_id.clone(),
+            body,
+            UpdateCSMThreatsAgentRuleOptionalParams::default().policy_id(policy_data_id.clone()),
+        )
         .await;
     if let Ok(value) = resp {
         println!("{:#?}", value);
