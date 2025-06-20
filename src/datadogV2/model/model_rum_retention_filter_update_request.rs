3a2844256542750d5939f0f67181bca732667131
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-20 20:25:30 +0000

    Regenerate client from commit 3fe1f086 of spec repo

--- src/datadogV2/model/model_rum_retention_filter_update_request.rs
+++ src/datadogV2/model/model_rum_retention_filter_update_request.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Deserializer, Serialize};
 use serde_with::skip_serializing_none;
 use std::fmt::{self, Formatter};
 
-/// The RUM retention filter body to update.
+/// The RUM retention filter body to update
 #[non_exhaustive]
 #[skip_serializing_none]
 #[derive(Clone, Debug, PartialEq, Serialize)]
