d9c1fe185cd84b4ebe0e2b7b0fb38acba1aaa734
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-02 16:40:38 +0000

    Regenerate client from commit de2517ad of spec repo

--- tests/scenarios/function_mappings.rs
+++ tests/scenarios/function_mappings.rs
@@ -2631,6 +2631,10 @@ pub fn collect_function_calls(world: &mut DatadogWorld) {
         test_v2_create_new_aws_external_id,
     );
     world.function_mappings.insert(
+        "v2.GetAWSIntegrationIAMPermissions".into(),
+        test_v2_get_aws_integration_iam_permissions,
+    );
+    world.function_mappings.insert(
         "v2.ListAWSLogsServices".into(),
         test_v2_list_aws_logs_services,
     );
@@ -19634,6 +19638,33 @@ fn test_v2_create_new_aws_external_id(
         .as_ref()
         .expect("api instance not found");
     let response = match block_on(api.create_new_aws_external_id_with_http_info()) {
+        Ok(response) => response,
+        Err(error) => {
+            return match error {
+                Error::ResponseError(e) => {
+                    world.response.code = e.status.as_u16();
+                    if let Some(entity) = e.entity {
+                        world.response.object = serde_json::to_value(entity).unwrap();
+                    }
+                }
+                _ => panic!("error parsing response: {error}"),
+            };
+        }
+    };
+    world.response.object = serde_json::to_value(response.entity).unwrap();
+    world.response.code = response.status.as_u16();
+}
+
+fn test_v2_get_aws_integration_iam_permissions(
+    world: &mut DatadogWorld,
+    _parameters: &HashMap<String, Value>,
+) {
+    let api = world
+        .api_instances
+        .v2_api_aws_integration
+        .as_ref()
+        .expect("api instance not found");
+    let response = match block_on(api.get_aws_integration_iam_permissions_with_http_info()) {
         Ok(response) => response,
         Err(error) => {
             return match error {
