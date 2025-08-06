a0a01cb48fe1f51714f2c5b419813eb25d86722f
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-06 19:36:53 +0000

    Regenerate client from commit 2bbbc7d of spec repo

--- src/datadogV2/model/model_observability_pipeline_config_destination_item.rs
+++ src/datadogV2/model/model_observability_pipeline_config_destination_item.rs
@@ -49,6 +49,9 @@ pub enum ObservabilityPipelineConfigDestinationItem {
     ObservabilityPipelineAmazonOpenSearchDestination(
         Box<crate::datadogV2::model::ObservabilityPipelineAmazonOpenSearchDestination>,
     ),
+    ObservabilityPipelineCrowdStrikeNextGenSiemDestination(
+        Box<crate::datadogV2::model::ObservabilityPipelineCrowdStrikeNextGenSiemDestination>,
+    ),
     UnparsedObject(crate::datadog::UnparsedObject),
 }
 
@@ -178,6 +181,14 @@ impl<'de> Deserialize<'de> for ObservabilityPipelineConfigDestinationItem {
         {
             if !_v._unparsed {
                 return Ok(ObservabilityPipelineConfigDestinationItem::ObservabilityPipelineAmazonOpenSearchDestination(_v));
+            }
+        }
+        if let Ok(_v) = serde_json::from_value::<
+            Box<crate::datadogV2::model::ObservabilityPipelineCrowdStrikeNextGenSiemDestination>,
+        >(value.clone())
+        {
+            if !_v._unparsed {
+                return Ok(ObservabilityPipelineConfigDestinationItem::ObservabilityPipelineCrowdStrikeNextGenSiemDestination(_v));
             }
         }
 
