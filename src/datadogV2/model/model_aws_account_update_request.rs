38e9f93bfc93c29d5d9cdfad8dbd1d797a73b372
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-25 18:50:49 +0000

    Regenerate client from commit 8726b93 of spec repo

--- src/datadogV2/model/model_aws_account_update_request.rs
+++ src/datadogV2/model/model_aws_account_update_request.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Deserializer, Serialize};
 use serde_with::skip_serializing_none;
 use std::fmt::{self, Formatter};
 
-/// AWS Account Update Request body.
+/// AWS Account update request body.
 #[non_exhaustive]
 #[skip_serializing_none]
 #[derive(Clone, Debug, PartialEq, Serialize)]
