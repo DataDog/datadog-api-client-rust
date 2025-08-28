f1a79b331a3a9fc2c01b7e9d6254a6d7a739bf91
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-28 12:02:53 +0000

    Regenerate client from commit 092c127 of spec repo

--- examples/v2_agentless-scanning_GetAwsScanOptions.rs
+++ examples/v2_agentless-scanning_GetAwsScanOptions.rs
@@ -1,12 +1,14 @@
-// List AWS Scan Options returns "OK" response
+// Get AWS scan options returns "OK" response
 use datadog_api_client::datadog;
 use datadog_api_client::datadogV2::api_agentless_scanning::AgentlessScanningAPI;
 
 #[tokio::main]
 async fn main() {
+    // there is a valid "aws_scan_options" in the system
+    let aws_scan_options_id = std::env::var("AWS_SCAN_OPTIONS_ID").unwrap();
     let configuration = datadog::Configuration::new();
     let api = AgentlessScanningAPI::with_config(configuration);
-    let resp = api.list_aws_scan_options().await;
+    let resp = api.get_aws_scan_options(aws_scan_options_id.clone()).await;
     if let Ok(value) = resp {
         println!("{:#?}", value);
     } else {
--- src/datadogV2/api/api_agentless_scanning.rs
+++ src/datadogV2/api/api_agentless_scanning.rs
@@ -42,6 +42,14 @@ pub enum GetAwsOnDemandTaskError {
     UnknownValue(serde_json::Value),
 }
 
+/// GetAwsScanOptionsError is a struct for typed errors of method [`AgentlessScanningAPI::get_aws_scan_options`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum GetAwsScanOptionsError {
+    APIErrorResponse(crate::datadogV2::model::APIErrorResponse),
+    UnknownValue(serde_json::Value),
+}
+
 /// ListAwsOnDemandTasksError is a struct for typed errors of method [`AgentlessScanningAPI::list_aws_on_demand_tasks`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -631,6 +639,115 @@ impl AgentlessScanningAPI {
             };
         } else {
             let local_entity: Option<GetAwsOnDemandTaskError> =
+                serde_json::from_str(&local_content).ok();
+            let local_error = datadog::ResponseContent {
+                status: local_status,
+                content: local_content,
+                entity: local_entity,
+            };
+            Err(datadog::Error::ResponseError(local_error))
+        }
+    }
+
+    /// Fetches the Agentless scan options for an activated account.
+    pub async fn get_aws_scan_options(
+        &self,
+        account_id: String,
+    ) -> Result<
+        crate::datadogV2::model::AwsScanOptionsResponse,
+        datadog::Error<GetAwsScanOptionsError>,
+    > {
+        match self.get_aws_scan_options_with_http_info(account_id).await {
+            Ok(response_content) => {
+                if let Some(e) = response_content.entity {
+                    Ok(e)
+                } else {
+                    Err(datadog::Error::Serde(serde::de::Error::custom(
+                        "response content was None",
+                    )))
+                }
+            }
+            Err(err) => Err(err),
+        }
+    }
+
+    /// Fetches the Agentless scan options for an activated account.
+    pub async fn get_aws_scan_options_with_http_info(
+        &self,
+        account_id: String,
+    ) -> Result<
+        datadog::ResponseContent<crate::datadogV2::model::AwsScanOptionsResponse>,
+        datadog::Error<GetAwsScanOptionsError>,
+    > {
+        let local_configuration = &self.config;
+        let operation_id = "v2.get_aws_scan_options";
+
+        let local_client = &self.client;
+
+        let local_uri_str = format!(
+            "{}/api/v2/agentless_scanning/accounts/aws/{account_id}",
+            local_configuration.get_operation_host(operation_id),
+            account_id = datadog::urlencode(account_id)
+        );
+        let mut local_req_builder =
+            local_client.request(reqwest::Method::GET, local_uri_str.as_str());
+
+        // build headers
+        let mut headers = HeaderMap::new();
+        headers.insert("Accept", HeaderValue::from_static("application/json"));
+
+        // build user agent
+        match HeaderValue::from_str(local_configuration.user_agent.as_str()) {
+            Ok(user_agent) => headers.insert(reqwest::header::USER_AGENT, user_agent),
+            Err(e) => {
+                log::warn!("Failed to parse user agent header: {e}, falling back to default");
+                headers.insert(
+                    reqwest::header::USER_AGENT,
+                    HeaderValue::from_static(datadog::DEFAULT_USER_AGENT.as_str()),
+                )
+            }
+        };
+
+        // build auth
+        if let Some(local_key) = local_configuration.auth_keys.get("apiKeyAuth") {
+            headers.insert(
+                "DD-API-KEY",
+                HeaderValue::from_str(local_key.key.as_str())
+                    .expect("failed to parse DD-API-KEY header"),
+            );
+        };
+        if let Some(local_key) = local_configuration.auth_keys.get("appKeyAuth") {
+            headers.insert(
+                "DD-APPLICATION-KEY",
+                HeaderValue::from_str(local_key.key.as_str())
+                    .expect("failed to parse DD-APPLICATION-KEY header"),
+            );
+        };
+
+        local_req_builder = local_req_builder.headers(headers);
+        let local_req = local_req_builder.build()?;
+        log::debug!("request content: {:?}", local_req.body());
+        let local_resp = local_client.execute(local_req).await?;
+
+        let local_status = local_resp.status();
+        let local_content = local_resp.text().await?;
+        log::debug!("response content: {}", local_content);
+
+        if !local_status.is_client_error() && !local_status.is_server_error() {
+            match serde_json::from_str::<crate::datadogV2::model::AwsScanOptionsResponse>(
+                &local_content,
+            ) {
+                Ok(e) => {
+                    return Ok(datadog::ResponseContent {
+                        status: local_status,
+                        content: local_content,
+                        entity: Some(e),
+                    })
+                }
+                Err(e) => return Err(datadog::Error::Serde(e)),
+            };
+        } else {
+            let local_entity: Option<GetAwsScanOptionsError> =
                 serde_json::from_str(&local_content).ok();
             let local_error = datadog::ResponseContent {
                 status: local_status,
