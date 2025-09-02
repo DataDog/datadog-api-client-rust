d3be83196593b22911c982d2a9529181fade5b67
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-02 17:59:01 +0000

    Regenerate client from commit f9eba94 of spec repo

--- tests/scenarios/function_mappings.rs
+++ tests/scenarios/function_mappings.rs
@@ -2198,6 +2198,14 @@ pub fn collect_function_calls(world: &mut DatadogWorld) {
         test_v2_create_security_monitoring_suppression,
     );
     world.function_mappings.insert(
+        "v2.GetSuppressionsAffectingFutureRule".into(),
+        test_v2_get_suppressions_affecting_future_rule,
+    );
+    world.function_mappings.insert(
+        "v2.GetSuppressionsAffectingRule".into(),
+        test_v2_get_suppressions_affecting_rule,
+    );
+    world.function_mappings.insert(
         "v2.DeleteSecurityMonitoringSuppression".into(),
         test_v2_delete_security_monitoring_suppression,
     );
@@ -15638,6 +15646,62 @@ fn test_v2_create_security_monitoring_suppression(
         .expect("api instance not found");
     let body = serde_json::from_value(_parameters.get("body").unwrap().clone()).unwrap();
     let response = match block_on(api.create_security_monitoring_suppression_with_http_info(body)) {
+        Ok(response) => response,
+        Err(error) => {
+            return match error {
+                Error::ResponseError(e) => {
+                    world.response.code = e.status.as_u16();
+                    if let Some(entity) = e.entity {
+                        world.response.object = serde_json::to_value(entity).unwrap();
+                    }
+                }
+                _ => panic!("error parsing response: {error}"),
+            };
+        }
+    };
+    world.response.object = serde_json::to_value(response.entity).unwrap();
+    world.response.code = response.status.as_u16();
+}
+
+fn test_v2_get_suppressions_affecting_future_rule(
+    world: &mut DatadogWorld,
+    _parameters: &HashMap<String, Value>,
+) {
+    let api = world
+        .api_instances
+        .v2_api_security_monitoring
+        .as_ref()
+        .expect("api instance not found");
+    let body = serde_json::from_value(_parameters.get("body").unwrap().clone()).unwrap();
+    let response = match block_on(api.get_suppressions_affecting_future_rule_with_http_info(body)) {
+        Ok(response) => response,
+        Err(error) => {
+            return match error {
+                Error::ResponseError(e) => {
+                    world.response.code = e.status.as_u16();
+                    if let Some(entity) = e.entity {
+                        world.response.object = serde_json::to_value(entity).unwrap();
+                    }
+                }
+                _ => panic!("error parsing response: {error}"),
+            };
+        }
+    };
+    world.response.object = serde_json::to_value(response.entity).unwrap();
+    world.response.code = response.status.as_u16();
+}
+
+fn test_v2_get_suppressions_affecting_rule(
+    world: &mut DatadogWorld,
+    _parameters: &HashMap<String, Value>,
+) {
+    let api = world
+        .api_instances
+        .v2_api_security_monitoring
+        .as_ref()
+        .expect("api instance not found");
+    let rule_id = serde_json::from_value(_parameters.get("rule_id").unwrap().clone()).unwrap();
+    let response = match block_on(api.get_suppressions_affecting_rule_with_http_info(rule_id)) {
         Ok(response) => response,
         Err(error) => {
             return match error {
