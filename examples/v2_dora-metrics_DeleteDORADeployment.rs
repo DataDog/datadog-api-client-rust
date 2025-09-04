7d25f67ae713806329463ab9b3d97819c61f2ef0
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-04 16:36:22 +0000

    Regenerate client from commit dd160d0 of spec repo

---
+++ examples/v2_dora-metrics_DeleteDORADeployment.rs
@@ -0,0 +1,17 @@
+// Delete a deployment event returns "Accepted" response
+use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_dora_metrics::DORAMetricsAPI;
+
+#[tokio::main]
+async fn main() {
+    let configuration = datadog::Configuration::new();
+    let api = DORAMetricsAPI::with_config(configuration);
+    let resp = api
+        .delete_dora_deployment("{unique_lower}".to_string())
+        .await;
+    if let Ok(value) = resp {
+        println!("{:#?}", value);
+    } else {
+        println!("{:#?}", resp.unwrap_err());
+    }
+}
