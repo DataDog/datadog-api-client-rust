71e118ce1ac451eb7dc9212749f5ae017f2a00c2
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-18 15:21:31 +0000

    Regenerate client from commit bd15e41 of spec repo

---
+++ src/datadogV2/model/model_observability_pipeline_opentelemetry_source.rs
@@ -0,0 +1,132 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::skip_serializing_none;
+use std::fmt::{self, Formatter};
+
+/// The `opentelemetry` source receives OpenTelemetry data through gRPC or HTTP.
+#[non_exhaustive]
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub struct ObservabilityPipelineOpentelemetrySource {
+    /// The unique identifier for this component. Used to reference this component in other parts of the pipeline (e.g., as input to downstream components).
+    #[serde(rename = "id")]
+    pub id: String,
+    /// Configuration for enabling TLS encryption between the pipeline component and external services.
+    #[serde(rename = "tls")]
+    pub tls: Option<crate::datadogV2::model::ObservabilityPipelineTls>,
+    /// The source type. The value should always be `opentelemetry`.
+    #[serde(rename = "type")]
+    pub type_: crate::datadogV2::model::ObservabilityPipelineOpentelemetrySourceType,
+    #[serde(flatten)]
+    pub additional_properties: std::collections::BTreeMap<String, serde_json::Value>,
+    #[serde(skip)]
+    #[serde(default)]
+    pub(crate) _unparsed: bool,
+}
+
+impl ObservabilityPipelineOpentelemetrySource {
+    pub fn new(
+        id: String,
+        type_: crate::datadogV2::model::ObservabilityPipelineOpentelemetrySourceType,
+    ) -> ObservabilityPipelineOpentelemetrySource {
+        ObservabilityPipelineOpentelemetrySource {
+            id,
+            tls: None,
+            type_,
+            additional_properties: std::collections::BTreeMap::new(),
+            _unparsed: false,
+        }
+    }
+
+    pub fn tls(mut self, value: crate::datadogV2::model::ObservabilityPipelineTls) -> Self {
+        self.tls = Some(value);
+        self
+    }
+
+    pub fn additional_properties(
+        mut self,
+        value: std::collections::BTreeMap<String, serde_json::Value>,
+    ) -> Self {
+        self.additional_properties = value;
+        self
+    }
+}
+
+impl<'de> Deserialize<'de> for ObservabilityPipelineOpentelemetrySource {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ObservabilityPipelineOpentelemetrySourceVisitor;
+        impl<'a> Visitor<'a> for ObservabilityPipelineOpentelemetrySourceVisitor {
+            type Value = ObservabilityPipelineOpentelemetrySource;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("a mapping")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'a>,
+            {
+                let mut id: Option<String> = None;
+                let mut tls: Option<crate::datadogV2::model::ObservabilityPipelineTls> = None;
+                let mut type_: Option<
+                    crate::datadogV2::model::ObservabilityPipelineOpentelemetrySourceType,
+                > = None;
+                let mut additional_properties: std::collections::BTreeMap<
+                    String,
+                    serde_json::Value,
+                > = std::collections::BTreeMap::new();
+                let mut _unparsed = false;
+
+                while let Some((k, v)) = map.next_entry::<String, serde_json::Value>()? {
+                    match k.as_str() {
+                        "id" => {
+                            id = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        "tls" => {
+                            if v.is_null() {
+                                continue;
+                            }
+                            tls = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        "type" => {
+                            type_ = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                            if let Some(ref _type_) = type_ {
+                                match _type_ {
+                                    crate::datadogV2::model::ObservabilityPipelineOpentelemetrySourceType::UnparsedObject(_type_) => {
+                                        _unparsed = true;
+                                    },
+                                    _ => {}
+                                }
+                            }
+                        }
+                        &_ => {
+                            if let Ok(value) = serde_json::from_value(v.clone()) {
+                                additional_properties.insert(k, value);
+                            }
+                        }
+                    }
+                }
+                let id = id.ok_or_else(|| M::Error::missing_field("id"))?;
+                let type_ = type_.ok_or_else(|| M::Error::missing_field("type_"))?;
+
+                let content = ObservabilityPipelineOpentelemetrySource {
+                    id,
+                    tls,
+                    type_,
+                    additional_properties,
+                    _unparsed,
+                };
+
+                Ok(content)
+            }
+        }
+
+        deserializer.deserialize_any(ObservabilityPipelineOpentelemetrySourceVisitor)
+    }
+}
