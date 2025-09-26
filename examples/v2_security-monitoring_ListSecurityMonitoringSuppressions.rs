e5c129ce67ea01b812884d50a9b048b8de9838cf
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-26 12:38:32 +0000

    Regenerate client from commit 090d92d of spec repo

--- examples/v2_security-monitoring_ListSecurityMonitoringSuppressions.rs
+++ examples/v2_security-monitoring_ListSecurityMonitoringSuppressions.rs
@@ -1,12 +1,17 @@
 // Get all suppression rules returns "OK" response
 use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_security_monitoring::ListSecurityMonitoringSuppressionsOptionalParams;
 use datadog_api_client::datadogV2::api_security_monitoring::SecurityMonitoringAPI;
 
 #[tokio::main]
 async fn main() {
     let configuration = datadog::Configuration::new();
     let api = SecurityMonitoringAPI::with_config(configuration);
-    let resp = api.list_security_monitoring_suppressions().await;
+    let resp = api
+        .list_security_monitoring_suppressions(
+            ListSecurityMonitoringSuppressionsOptionalParams::default(),
+        )
+        .await;
     if let Ok(value) = resp {
         println!("{:#?}", value);
     } else {
