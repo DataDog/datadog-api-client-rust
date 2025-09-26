ee6b901148cd13f5b3bdd195e8cf0665863cb8ae
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-26 21:22:28 +0000

    Regenerate client from commit 2bf1795 of spec repo

---
+++ src/datadogV2/model/model_resolve_vulnerable_symbols_request_data_attributes.rs
@@ -0,0 +1,105 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::skip_serializing_none;
+use std::fmt::{self, Formatter};
+
+/// The definition of `ResolveVulnerableSymbolsRequestDataAttributes` object.
+#[non_exhaustive]
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub struct ResolveVulnerableSymbolsRequestDataAttributes {
+    /// The `attributes` `purls`.
+    #[serde(rename = "purls")]
+    pub purls: Option<Vec<String>>,
+    #[serde(flatten)]
+    pub additional_properties: std::collections::BTreeMap<String, serde_json::Value>,
+    #[serde(skip)]
+    #[serde(default)]
+    pub(crate) _unparsed: bool,
+}
+
+impl ResolveVulnerableSymbolsRequestDataAttributes {
+    pub fn new() -> ResolveVulnerableSymbolsRequestDataAttributes {
+        ResolveVulnerableSymbolsRequestDataAttributes {
+            purls: None,
+            additional_properties: std::collections::BTreeMap::new(),
+            _unparsed: false,
+        }
+    }
+
+    pub fn purls(mut self, value: Vec<String>) -> Self {
+        self.purls = Some(value);
+        self
+    }
+
+    pub fn additional_properties(
+        mut self,
+        value: std::collections::BTreeMap<String, serde_json::Value>,
+    ) -> Self {
+        self.additional_properties = value;
+        self
+    }
+}
+
+impl Default for ResolveVulnerableSymbolsRequestDataAttributes {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'de> Deserialize<'de> for ResolveVulnerableSymbolsRequestDataAttributes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ResolveVulnerableSymbolsRequestDataAttributesVisitor;
+        impl<'a> Visitor<'a> for ResolveVulnerableSymbolsRequestDataAttributesVisitor {
+            type Value = ResolveVulnerableSymbolsRequestDataAttributes;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("a mapping")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'a>,
+            {
+                let mut purls: Option<Vec<String>> = None;
+                let mut additional_properties: std::collections::BTreeMap<
+                    String,
+                    serde_json::Value,
+                > = std::collections::BTreeMap::new();
+                let mut _unparsed = false;
+
+                while let Some((k, v)) = map.next_entry::<String, serde_json::Value>()? {
+                    match k.as_str() {
+                        "purls" => {
+                            if v.is_null() {
+                                continue;
+                            }
+                            purls = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        &_ => {
+                            if let Ok(value) = serde_json::from_value(v.clone()) {
+                                additional_properties.insert(k, value);
+                            }
+                        }
+                    }
+                }
+
+                let content = ResolveVulnerableSymbolsRequestDataAttributes {
+                    purls,
+                    additional_properties,
+                    _unparsed,
+                };
+
+                Ok(content)
+            }
+        }
+
+        deserializer.deserialize_any(ResolveVulnerableSymbolsRequestDataAttributesVisitor)
+    }
+}
---
+++ src/datadogV2/model/model_sca_request_data_attributes_relations_items.rs
@@ -0,0 +1,122 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::skip_serializing_none;
+use std::fmt::{self, Formatter};
+
+/// The definition of `ScaRequestDataAttributesRelationsItems` object.
+#[non_exhaustive]
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub struct ScaRequestDataAttributesRelationsItems {
+    /// The `items` `depends_on`.
+    #[serde(rename = "depends_on")]
+    pub depends_on: Option<Vec<String>>,
+    /// The `items` `ref`.
+    #[serde(rename = "ref")]
+    pub ref_: Option<String>,
+    #[serde(flatten)]
+    pub additional_properties: std::collections::BTreeMap<String, serde_json::Value>,
+    #[serde(skip)]
+    #[serde(default)]
+    pub(crate) _unparsed: bool,
+}
+
+impl ScaRequestDataAttributesRelationsItems {
+    pub fn new() -> ScaRequestDataAttributesRelationsItems {
+        ScaRequestDataAttributesRelationsItems {
+            depends_on: None,
+            ref_: None,
+            additional_properties: std::collections::BTreeMap::new(),
+            _unparsed: false,
+        }
+    }
+
+    pub fn depends_on(mut self, value: Vec<String>) -> Self {
+        self.depends_on = Some(value);
+        self
+    }
+
+    pub fn ref_(mut self, value: String) -> Self {
+        self.ref_ = Some(value);
+        self
+    }
+
+    pub fn additional_properties(
+        mut self,
+        value: std::collections::BTreeMap<String, serde_json::Value>,
+    ) -> Self {
+        self.additional_properties = value;
+        self
+    }
+}
+
+impl Default for ScaRequestDataAttributesRelationsItems {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'de> Deserialize<'de> for ScaRequestDataAttributesRelationsItems {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ScaRequestDataAttributesRelationsItemsVisitor;
+        impl<'a> Visitor<'a> for ScaRequestDataAttributesRelationsItemsVisitor {
+            type Value = ScaRequestDataAttributesRelationsItems;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("a mapping")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'a>,
+            {
+                let mut depends_on: Option<Vec<String>> = None;
+                let mut ref_: Option<String> = None;
+                let mut additional_properties: std::collections::BTreeMap<
+                    String,
+                    serde_json::Value,
+                > = std::collections::BTreeMap::new();
+                let mut _unparsed = false;
+
+                while let Some((k, v)) = map.next_entry::<String, serde_json::Value>()? {
+                    match k.as_str() {
+                        "depends_on" => {
+                            if v.is_null() {
+                                continue;
+                            }
+                            depends_on = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        "ref" => {
+                            if v.is_null() {
+                                continue;
+                            }
+                            ref_ = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        &_ => {
+                            if let Ok(value) = serde_json::from_value(v.clone()) {
+                                additional_properties.insert(k, value);
+                            }
+                        }
+                    }
+                }
+
+                let content = ScaRequestDataAttributesRelationsItems {
+                    depends_on,
+                    ref_,
+                    additional_properties,
+                    _unparsed,
+                };
+
+                Ok(content)
+            }
+        }
+
+        deserializer.deserialize_any(ScaRequestDataAttributesRelationsItemsVisitor)
+    }
+}
