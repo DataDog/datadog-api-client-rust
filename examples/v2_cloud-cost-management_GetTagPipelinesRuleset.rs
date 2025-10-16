de8bac3752beda6c14b93ed2ca74b30d41e15c1a
ci.datadog-api-spec <packages@datadoghq.com>
2025-10-16 09:06:33 +0000

    Regenerate client from commit 673f988 of spec repo

--- examples/v2_cloud-cost-management_GetTagPipelinesRuleset.rs
+++ examples/v2_cloud-cost-management_GetTagPipelinesRuleset.rs
@@ -1,4 +1,4 @@
-// Get ruleset returns "OK" response
+// Get a tag pipeline ruleset returns "OK" response
 use datadog_api_client::datadog;
 use datadog_api_client::datadogV2::api_cloud_cost_management::CloudCostManagementAPI;
 
@@ -7,7 +7,7 @@ async fn main() {
     let configuration = datadog::Configuration::new();
     let api = CloudCostManagementAPI::with_config(configuration);
     let resp = api
-        .get_tag_pipelines_ruleset("ruleset_id".to_string())
+        .get_tag_pipelines_ruleset("a1e9de9b-b88e-41c6-a0cd-cc0ebd7092de".to_string())
         .await;
     if let Ok(value) = resp {
         println!("{:#?}", value);
