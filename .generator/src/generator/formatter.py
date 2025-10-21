fbfac1d0efcc95a9f648b158e27e497e5037e81f
jack-edmonds-dd <jack.edmonds@datadoghq.com>
2025-10-21 13:01:59 -0400

    Allow missing descriptions.

--- .generator/src/generator/formatter.py
+++ .generator/src/generator/formatter.py
@@ -70,7 +70,7 @@ def is_primitive(schema):
 
 
 def block_comment(comment, prefix="///", first_line=True):
-    comment = re.sub(r"(\w+://[^\s\(\)]+)", r"<\1>", comment)
+    comment = re.sub(r"(\w+://[^\s\(\)]+)", r"<\1>", comment or "")
     lines = comment.split("\n")
     start = "" if first_line else lines[0] + "\n"
     return (start + "\n".join(f"{prefix} {line}".rstrip() for line in lines[(0 if first_line else 1) :])).rstrip()
