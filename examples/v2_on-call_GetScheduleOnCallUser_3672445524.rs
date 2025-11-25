d4f7aa640bc68b38997ac6faaa76448177237a5e
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-25 17:50:01 +0000

    Regenerate client from commit 479ca66 of spec repo

--- examples/v2_on-call_GetScheduleOnCallUser_3672445524.rs
+++ examples/v2_on-call_GetScheduleOnCallUser_3672445524.rs
@@ -1,15 +1,17 @@
-// Get the scheduled on-call user returns "OK" response
+// Get the schedule on-call user returns "OK" response
 use datadog_api_client::datadog;
 use datadog_api_client::datadogV2::api_on_call::GetScheduleOnCallUserOptionalParams;
 use datadog_api_client::datadogV2::api_on_call::OnCallAPI;
 
 #[tokio::main]
 async fn main() {
+    // there is a valid "schedule" in the system
+    let schedule_data_id = std::env::var("SCHEDULE_DATA_ID").unwrap();
     let configuration = datadog::Configuration::new();
     let api = OnCallAPI::with_config(configuration);
     let resp = api
         .get_schedule_on_call_user(
-            "3653d3c6-0c75-11ea-ad28-fb5701eabc7d".to_string(),
+            schedule_data_id.clone(),
             GetScheduleOnCallUserOptionalParams::default(),
         )
         .await;
