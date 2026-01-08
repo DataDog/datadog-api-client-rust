4f707be4cf032197d9c2529a1cac0e9cfb3d897f
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-08 14:27:51 +0000

    Regenerate client from commit 9f53851 of spec repo

---
+++ examples/v2_synthetics_CreateSyntheticsSuite.rs
@@ -0,0 +1,32 @@
+// Synthetics: Create a test suite returns "OK" response
+use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_synthetics::SyntheticsAPI;
+use datadog_api_client::datadogV2::model::SuiteCreateEdit;
+use datadog_api_client::datadogV2::model::SuiteCreateEditRequest;
+use datadog_api_client::datadogV2::model::SyntheticsSuite;
+use datadog_api_client::datadogV2::model::SyntheticsSuiteOptions;
+use datadog_api_client::datadogV2::model::SyntheticsSuiteType;
+use datadog_api_client::datadogV2::model::SyntheticsSuiteTypes;
+
+#[tokio::main]
+async fn main() {
+    let body = SuiteCreateEditRequest::new(SuiteCreateEdit::new(
+        SyntheticsSuite::new(
+            "Notification message".to_string(),
+            "Example suite name".to_string(),
+            SyntheticsSuiteOptions::new(),
+            vec![],
+            SyntheticsSuiteType::SUITE,
+        )
+        .tags(vec!["env:production".to_string()]),
+        SyntheticsSuiteTypes::SUITES,
+    ));
+    let configuration = datadog::Configuration::new();
+    let api = SyntheticsAPI::with_config(configuration);
+    let resp = api.create_synthetics_suite(body).await;
+    if let Ok(value) = resp {
+        println!("{:#?}", value);
+    } else {
+        println!("{:#?}", resp.unwrap_err());
+    }
+}
