91896f8f06c827571f5b9bd7b5dd5c345d51865d
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-21 10:44:49 +0000

    Regenerate client from commit 9fb9d74 of spec repo

---
+++ src/datadogV2/model/model_deleted_suites_request_delete_request.rs
@@ -0,0 +1,92 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::skip_serializing_none;
+use std::fmt::{self, Formatter};
+
+#[non_exhaustive]
+#[skip_serializing_none]
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub struct DeletedSuitesRequestDeleteRequest {
+    #[serde(rename = "data")]
+    pub data: crate::datadogV2::model::DeletedSuitesRequestDelete,
+    #[serde(flatten)]
+    pub additional_properties: std::collections::BTreeMap<String, serde_json::Value>,
+    #[serde(skip)]
+    #[serde(default)]
+    pub(crate) _unparsed: bool,
+}
+
+impl DeletedSuitesRequestDeleteRequest {
+    pub fn new(
+        data: crate::datadogV2::model::DeletedSuitesRequestDelete,
+    ) -> DeletedSuitesRequestDeleteRequest {
+        DeletedSuitesRequestDeleteRequest {
+            data,
+            additional_properties: std::collections::BTreeMap::new(),
+            _unparsed: false,
+        }
+    }
+
+    pub fn additional_properties(
+        mut self,
+        value: std::collections::BTreeMap<String, serde_json::Value>,
+    ) -> Self {
+        self.additional_properties = value;
+        self
+    }
+}
+
+impl<'de> Deserialize<'de> for DeletedSuitesRequestDeleteRequest {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct DeletedSuitesRequestDeleteRequestVisitor;
+        impl<'a> Visitor<'a> for DeletedSuitesRequestDeleteRequestVisitor {
+            type Value = DeletedSuitesRequestDeleteRequest;
+
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("a mapping")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'a>,
+            {
+                let mut data: Option<crate::datadogV2::model::DeletedSuitesRequestDelete> = None;
+                let mut additional_properties: std::collections::BTreeMap<
+                    String,
+                    serde_json::Value,
+                > = std::collections::BTreeMap::new();
+                let mut _unparsed = false;
+
+                while let Some((k, v)) = map.next_entry::<String, serde_json::Value>()? {
+                    match k.as_str() {
+                        "data" => {
+                            data = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        &_ => {
+                            if let Ok(value) = serde_json::from_value(v.clone()) {
+                                additional_properties.insert(k, value);
+                            }
+                        }
+                    }
+                }
+                let data = data.ok_or_else(|| M::Error::missing_field("data"))?;
+
+                let content = DeletedSuitesRequestDeleteRequest {
+                    data,
+                    additional_properties,
+                    _unparsed,
+                };
+
+                Ok(content)
+            }
+        }
+
+        deserializer.deserialize_any(DeletedSuitesRequestDeleteRequestVisitor)
+    }
+}
