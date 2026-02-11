d5c62c1653dd798049758a8e771af370eab245dc
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-11 16:47:10 +0000

    Regenerate client from commit 0cec205 of spec repo

--- examples/v2_roles_CreateRole.rs
+++ examples/v2_roles_CreateRole.rs
@@ -13,13 +13,15 @@ use datadog_api_client::datadogV2::model::RolesType;
 #[tokio::main]
 async fn main() {
     let body = RoleCreateRequest::new(
-        RoleCreateData::new(RoleCreateAttributes::new("developers".to_string()))
-            .relationships(RoleRelationships::new().permissions(
-                RelationshipToPermissions::new().data(vec![
-                    RelationshipToPermissionData::new().type_(PermissionsType::PERMISSIONS),
-                ]),
-            ))
-            .type_(RolesType::ROLES),
+        RoleCreateData::new(
+            RoleCreateAttributes::new("developers".to_string()).receives_permissions_from(vec![]),
+        )
+        .relationships(
+            RoleRelationships::new().permissions(RelationshipToPermissions::new().data(vec![
+                RelationshipToPermissionData::new().type_(PermissionsType::PERMISSIONS),
+            ])),
+        )
+        .type_(RolesType::ROLES),
     );
     let configuration = datadog::Configuration::new();
     let api = RolesAPI::with_config(configuration);
