78df8fc3a7f8017e31c52442e290f392a7d9e4ca
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-17 08:42:07 +0000

    Regenerate client from commit 9560e24 of spec repo

--- examples/v2_teams_UpdateTeamMembership.rs
+++ examples/v2_teams_UpdateTeamMembership.rs
@@ -1,5 +1,4 @@
-// Update a user's membership attributes on a team returns "Represents a user's
-// association to a team" response
+// Update a user's membership attributes on a team returns "OK" response
 use datadog_api_client::datadog;
 use datadog_api_client::datadogV2::api_teams::TeamsAPI;
 use datadog_api_client::datadogV2::model::UserTeamAttributes;
@@ -10,6 +9,11 @@ use datadog_api_client::datadogV2::model::UserTeamUpdateRequest;
 
 #[tokio::main]
 async fn main() {
+    // there is a valid "dd_team" in the system
+    let dd_team_data_id = std::env::var("DD_TEAM_DATA_ID").unwrap();
+
+    // there is a valid "user" in the system
+    let user_data_id = std::env::var("USER_DATA_ID").unwrap();
     let body = UserTeamUpdateRequest::new(
         UserTeamUpdate::new(UserTeamType::TEAM_MEMBERSHIPS)
             .attributes(UserTeamAttributes::new().role(Some(UserTeamRole::ADMIN))),
@@ -17,7 +21,7 @@ async fn main() {
     let configuration = datadog::Configuration::new();
     let api = TeamsAPI::with_config(configuration);
     let resp = api
-        .update_team_membership("team_id".to_string(), "user_id".to_string(), body)
+        .update_team_membership(dd_team_data_id.clone(), user_data_id.clone(), body)
         .await;
     if let Ok(value) = resp {
         println!("{:#?}", value);
