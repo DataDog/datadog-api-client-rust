27e1cebc819c0d8ee90109ab0dd9fb0508f3b25e
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-11 17:05:15 +0000

    Regenerate client from commit af83973 of spec repo

--- src/datadogV2/model/model_hamr_org_connection_response.rs
+++ src/datadogV2/model/model_hamr_org_connection_response.rs
@@ -6,10 +6,12 @@ use serde::{Deserialize, Deserializer, Serialize};
 use serde_with::skip_serializing_none;
 use std::fmt::{self, Formatter};
 
+/// Response payload for a HAMR organization connection.
 #[non_exhaustive]
 #[skip_serializing_none]
 #[derive(Clone, Debug, PartialEq, Serialize)]
 pub struct HamrOrgConnectionResponse {
+    /// Data object for a HAMR organization connection response.
     #[serde(rename = "data")]
     pub data: crate::datadogV2::model::HamrOrgConnectionDataResponse,
     #[serde(flatten)]
