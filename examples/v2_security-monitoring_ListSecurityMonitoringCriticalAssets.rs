730d087e0bc3818750236d8592b36a78161fcc46
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-18 19:06:03 +0000

    Regenerate client from commit 5ca116c of spec repo

--- examples/v2_security-monitoring_ListSecurityMonitoringCriticalAssets.rs
+++ examples/v2_security-monitoring_ListSecurityMonitoringCriticalAssets.rs
@@ -1,12 +1,17 @@
 // Get all critical assets returns "OK" response
 use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_security_monitoring::ListSecurityMonitoringCriticalAssetsOptionalParams;
 use datadog_api_client::datadogV2::api_security_monitoring::SecurityMonitoringAPI;
 
 #[tokio::main]
 async fn main() {
     let configuration = datadog::Configuration::new();
     let api = SecurityMonitoringAPI::with_config(configuration);
-    let resp = api.list_security_monitoring_critical_assets().await;
+    let resp = api
+        .list_security_monitoring_critical_assets(
+            ListSecurityMonitoringCriticalAssetsOptionalParams::default(),
+        )
+        .await;
     if let Ok(value) = resp {
         println!("{:#?}", value);
     } else {
