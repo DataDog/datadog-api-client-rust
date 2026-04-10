8bfb3c49d006e043a73f5bbd0d6fc3fc6c7beb78
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-10 17:29:18 +0000

    Regenerate client from commit 26e9a4b of spec repo

---
+++ src/datadogV2/model/model_security_monitoring_terraform_resource_type.rs
@@ -0,0 +1,51 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+#[non_exhaustive]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum SecurityMonitoringTerraformResourceType {
+    SUPPRESSIONS,
+    CRITICAL_ASSETS,
+    UnparsedObject(crate::datadog::UnparsedObject),
+}
+
+impl ToString for SecurityMonitoringTerraformResourceType {
+    fn to_string(&self) -> String {
+        match self {
+            Self::SUPPRESSIONS => String::from("suppressions"),
+            Self::CRITICAL_ASSETS => String::from("critical_assets"),
+            Self::UnparsedObject(v) => v.value.to_string(),
+        }
+    }
+}
+
+impl Serialize for SecurityMonitoringTerraformResourceType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Self::UnparsedObject(v) => v.serialize(serializer),
+            _ => serializer.serialize_str(self.to_string().as_str()),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for SecurityMonitoringTerraformResourceType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: String = String::deserialize(deserializer)?;
+        Ok(match s.as_str() {
+            "suppressions" => Self::SUPPRESSIONS,
+            "critical_assets" => Self::CRITICAL_ASSETS,
+            _ => Self::UnparsedObject(crate::datadog::UnparsedObject {
+                value: serde_json::Value::String(s.into()),
+            }),
+        })
+    }
+}
