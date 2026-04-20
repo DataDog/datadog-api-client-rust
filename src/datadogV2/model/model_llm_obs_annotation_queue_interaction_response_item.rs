5cb640c3aeda023d2e683b9823d34f86353b0b71
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-20 19:43:16 +0000

    Regenerate client from commit 1dfb39b of spec repo

--- src/datadogV2/model/model_llm_obs_annotation_queue_interaction_response_item.rs
+++ src/datadogV2/model/model_llm_obs_annotation_queue_interaction_response_item.rs
@@ -17,9 +17,15 @@ pub struct LLMObsAnnotationQueueInteractionResponseItem {
     /// Identifier of the content for this interaction.
     #[serde(rename = "content_id")]
     pub content_id: String,
+    /// Timestamp when the interaction was added to the queue.
+    #[serde(rename = "created_at")]
+    pub created_at: chrono::DateTime<chrono::Utc>,
     /// Unique identifier of the interaction.
     #[serde(rename = "id")]
     pub id: String,
+    /// Timestamp when the interaction was last updated.
+    #[serde(rename = "modified_at")]
+    pub modified_at: chrono::DateTime<chrono::Utc>,
     /// Type of interaction in an annotation queue.
     #[serde(rename = "type")]
     pub type_: crate::datadogV2::model::LLMObsInteractionType,
@@ -34,13 +40,17 @@ impl LLMObsAnnotationQueueInteractionResponseItem {
     pub fn new(
         already_existed: bool,
         content_id: String,
+        created_at: chrono::DateTime<chrono::Utc>,
         id: String,
+        modified_at: chrono::DateTime<chrono::Utc>,
         type_: crate::datadogV2::model::LLMObsInteractionType,
     ) -> LLMObsAnnotationQueueInteractionResponseItem {
         LLMObsAnnotationQueueInteractionResponseItem {
             already_existed,
             content_id,
+            created_at,
             id,
+            modified_at,
             type_,
             additional_properties: std::collections::BTreeMap::new(),
             _unparsed: false,
@@ -75,7 +85,9 @@ impl<'de> Deserialize<'de> for LLMObsAnnotationQueueInteractionResponseItem {
             {
                 let mut already_existed: Option<bool> = None;
                 let mut content_id: Option<String> = None;
+                let mut created_at: Option<chrono::DateTime<chrono::Utc>> = None;
                 let mut id: Option<String> = None;
+                let mut modified_at: Option<chrono::DateTime<chrono::Utc>> = None;
                 let mut type_: Option<crate::datadogV2::model::LLMObsInteractionType> = None;
                 let mut additional_properties: std::collections::BTreeMap<
                     String,
@@ -92,8 +104,15 @@ impl<'de> Deserialize<'de> for LLMObsAnnotationQueueInteractionResponseItem {
                         "content_id" => {
                             content_id = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
                         }
+                        "created_at" => {
+                            created_at = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
                         "id" => {
                             id = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
+                        }
+                        "modified_at" => {
+                            modified_at =
+                                Some(serde_json::from_value(v).map_err(M::Error::custom)?);
                         }
                         "type" => {
                             type_ = Some(serde_json::from_value(v).map_err(M::Error::custom)?);
@@ -116,13 +135,18 @@ impl<'de> Deserialize<'de> for LLMObsAnnotationQueueInteractionResponseItem {
                 let already_existed =
                     already_existed.ok_or_else(|| M::Error::missing_field("already_existed"))?;
                 let content_id = content_id.ok_or_else(|| M::Error::missing_field("content_id"))?;
+                let created_at = created_at.ok_or_else(|| M::Error::missing_field("created_at"))?;
                 let id = id.ok_or_else(|| M::Error::missing_field("id"))?;
+                let modified_at =
+                    modified_at.ok_or_else(|| M::Error::missing_field("modified_at"))?;
                 let type_ = type_.ok_or_else(|| M::Error::missing_field("type_"))?;
 
                 let content = LLMObsAnnotationQueueInteractionResponseItem {
                     already_existed,
                     content_id,
+                    created_at,
                     id,
+                    modified_at,
                     type_,
                     additional_properties,
                     _unparsed,
