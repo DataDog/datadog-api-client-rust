6a32e9e5d6f5c8b037ab6fdd56ac52c7dbc56817
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-23 17:07:48 +0000

    Regenerate client from commit 2d6272d of spec repo

---
+++ examples/v2_security-monitoring_MuteSecurityFindings.rs
@@ -0,0 +1,40 @@
+// Mute or unmute security findings returns "Accepted" response
+use datadog_api_client::datadog;
+use datadog_api_client::datadogV2::api_security_monitoring::SecurityMonitoringAPI;
+use datadog_api_client::datadogV2::model::FindingData;
+use datadog_api_client::datadogV2::model::FindingDataType;
+use datadog_api_client::datadogV2::model::Findings;
+use datadog_api_client::datadogV2::model::MuteDataType;
+use datadog_api_client::datadogV2::model::MuteFindingsMuteAttributes;
+use datadog_api_client::datadogV2::model::MuteFindingsReason;
+use datadog_api_client::datadogV2::model::MuteFindingsRequest;
+use datadog_api_client::datadogV2::model::MuteFindingsRequestData;
+use datadog_api_client::datadogV2::model::MuteFindingsRequestDataAttributes;
+use datadog_api_client::datadogV2::model::MuteFindingsRequestDataRelationships;
+
+#[tokio::main]
+async fn main() {
+    let body = MuteFindingsRequest::new(
+        MuteFindingsRequestData::new(MuteDataType::MUTE)
+            .attributes(MuteFindingsRequestDataAttributes::new(
+                MuteFindingsMuteAttributes::new(true, MuteFindingsReason::PENDING_FIX)
+                    .description("To be resolved later.".to_string())
+                    .expiration_date(1778721573794),
+            ))
+            .id("93bfeb70-af47-424d-908a-948d3f08e37f".to_string())
+            .relationships(MuteFindingsRequestDataRelationships::new(
+                Findings::new().data(vec![FindingData::new(
+                    "ZGVmLTAwcC1pZXJ-aS0wZjhjNjMyZDNmMzRlZTgzNw==".to_string(),
+                    FindingDataType::FINDINGS,
+                )]),
+            )),
+    );
+    let configuration = datadog::Configuration::new();
+    let api = SecurityMonitoringAPI::with_config(configuration);
+    let resp = api.mute_security_findings(body).await;
+    if let Ok(value) = resp {
+        println!("{:#?}", value);
+    } else {
+        println!("{:#?}", resp.unwrap_err());
+    }
+}
