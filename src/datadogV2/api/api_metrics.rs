34fd58817e90a5982f1835059fb807986c687529
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-13 20:28:52 +0000

    Regenerate client from commit 8c9b068 of spec repo

--- src/datadogV2/api/api_metrics.rs
+++ src/datadogV2/api/api_metrics.rs
@@ -225,6 +225,24 @@ impl ListTagsByMetricNameOptionalParams {
     }
 }
 
+/// ListVolumesByMetricNameOptionalParams is a struct for passing parameters to the method [`MetricsAPI::list_volumes_by_metric_name`]
+#[non_exhaustive]
+#[derive(Clone, Default, Debug)]
+pub struct ListVolumesByMetricNameOptionalParams {
+    /// The number of seconds of look back (from now).
+    /// Default value is 3,600 (1 hour), maximum value is 2,592,000 (1 month).
+    pub window_seconds: Option<i64>,
+}
+
+impl ListVolumesByMetricNameOptionalParams {
+    /// The number of seconds of look back (from now).
+    /// Default value is 3,600 (1 hour), maximum value is 2,592,000 (1 month).
+    pub fn window_seconds(mut self, value: i64) -> Self {
+        self.window_seconds = Some(value);
+        self
+    }
+}
+
 /// SubmitMetricsOptionalParams is a struct for passing parameters to the method [`MetricsAPI::submit_metrics`]
 #[non_exhaustive]
 #[derive(Clone, Default, Debug)]
@@ -2020,18 +2038,19 @@ impl MetricsAPI {
         }
     }
 
-    /// View distinct metrics volumes for the given metric name.
+    /// View hourly average metric volumes for the given metric name over the look back period.
     ///
     /// Custom metrics generated in-app from other products will return `null` for ingested volumes.
     pub async fn list_volumes_by_metric_name(
         &self,
         metric_name: String,
+        params: ListVolumesByMetricNameOptionalParams,
     ) -> Result<
         crate::datadogV2::model::MetricVolumesResponse,
         datadog::Error<ListVolumesByMetricNameError>,
     > {
         match self
-            .list_volumes_by_metric_name_with_http_info(metric_name)
+            .list_volumes_by_metric_name_with_http_info(metric_name, params)
             .await
         {
             Ok(response_content) => {
@@ -2047,18 +2066,22 @@ impl MetricsAPI {
         }
     }
 
-    /// View distinct metrics volumes for the given metric name.
+    /// View hourly average metric volumes for the given metric name over the look back period.
     ///
     /// Custom metrics generated in-app from other products will return `null` for ingested volumes.
     pub async fn list_volumes_by_metric_name_with_http_info(
         &self,
         metric_name: String,
+        params: ListVolumesByMetricNameOptionalParams,
     ) -> Result<
         datadog::ResponseContent<crate::datadogV2::model::MetricVolumesResponse>,
         datadog::Error<ListVolumesByMetricNameError>,
     > {
         let local_configuration = &self.config;
         let operation_id = "v2.list_volumes_by_metric_name";
+
+        // unbox and build optional parameters
+        let window_seconds = params.window_seconds;
 
         let local_client = &self.client;
 
@@ -2069,6 +2092,11 @@ impl MetricsAPI {
         );
         let mut local_req_builder =
             local_client.request(reqwest::Method::GET, local_uri_str.as_str());
+
+        if let Some(ref local_query_param) = window_seconds {
+            local_req_builder =
+                local_req_builder.query(&[("window[seconds]", &local_query_param.to_string())]);
+        };
 
         // build headers
         let mut headers = HeaderMap::new();
