6e63a8f4e58bd5e5659085dcf8ec506814997d7e
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-30 20:23:01 +0000

    Regenerate client from commit a73640a of spec repo

---
+++ src/datadogV1/model/model_list_stream_query_version.rs
@@ -0,0 +1,48 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+#[non_exhaustive]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum ListStreamQueryVersion {
+    SEQUENTIAL_QUERY,
+    UnparsedObject(crate::datadog::UnparsedObject),
+}
+
+impl ToString for ListStreamQueryVersion {
+    fn to_string(&self) -> String {
+        match self {
+            Self::SEQUENTIAL_QUERY => String::from("sequential_query"),
+            Self::UnparsedObject(v) => v.value.to_string(),
+        }
+    }
+}
+
+impl Serialize for ListStreamQueryVersion {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Self::UnparsedObject(v) => v.serialize(serializer),
+            _ => serializer.serialize_str(self.to_string().as_str()),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for ListStreamQueryVersion {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: String = String::deserialize(deserializer)?;
+        Ok(match s.as_str() {
+            "sequential_query" => Self::SEQUENTIAL_QUERY,
+            _ => Self::UnparsedObject(crate::datadog::UnparsedObject {
+                value: serde_json::Value::String(s.into()),
+            }),
+        })
+    }
+}
