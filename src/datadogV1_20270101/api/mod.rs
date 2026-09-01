80d9b3318e8c4e22ad9e927a99b6afad428847b7
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-01 09:13:08 +0000

    Regenerate client from commit 3445017 of spec repo

--- src/datadogV1_20270101/api/mod.rs
+++ src/datadogV1_20270101/api/mod.rs
@@ -1,11 +1,5 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-Present Datadog, Inc.
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![doc = include_str!("../README.md")]
 
-pub mod datadog;
-pub mod datadogV1;
-pub mod datadogV1_20270101;
-pub mod datadogV2;
+pub mod api_dashboards;
