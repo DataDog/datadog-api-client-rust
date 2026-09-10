67c54deece19c29886e50d47586870a011c81a77
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-10 17:25:29 +0000

    Regenerate client from commit c3f76df of spec repo

--- src/datadogV2/model/model_ticket_creation_rule_action.rs
+++ src/datadogV2/model/model_ticket_creation_rule_action.rs
@@ -14,7 +14,11 @@ pub struct TicketCreationRuleAction {
     /// The UUID of the default assignee for created tickets.
     #[serde(rename = "assignee_id")]
     pub assignee_id: Option<uuid::Uuid>,
-    /// Custom fields of the Jira issue to create. For the list of available fields, see [Jira documentation](<https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-createmeta-projectidorkey-issuetypes-issuetypeid-get>).
+    /// Target-specific fields of the ticket to create.
+    ///
+    /// For `target: jira`, the custom fields of the Jira issue. For the list of available fields, see [Jira documentation](<https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-createmeta-projectidorkey-issuetypes-issuetypeid-get>).
+    ///
+    /// For `target: linear`, the optional keys `linear_project_id` (string, the identifier of the Linear project the issue is created in) and `linear_label_ids` (array of strings, the identifiers of the Linear labels applied to the issue).
     #[serde(rename = "fields")]
     pub fields: Option<std::collections::BTreeMap<String, serde_json::Value>>,
     /// The maximum number of tickets the rule may create per day. If exceeded, one final ticket will be created, explaining the limit was hit and link back to the responsible rule.
