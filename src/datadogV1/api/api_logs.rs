c823c678032e0d39b60b7bb4d14a267f24b9a8f8
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-18 14:25:44 +0000

    Regenerate client from commit f23e221 of spec repo

--- src/datadogV1/api/api_logs.rs
+++ src/datadogV1/api/api_logs.rs
@@ -363,6 +363,11 @@ impl LogsAPI {
     > {
         let local_configuration = &self.config;
         let local_operation_id = "v1.submit_log";
+        let params = if params.content_encoding.is_none() {
+            params.content_encoding(crate::datadogV1::model::ContentEncoding::GZIP)
+        } else {
+            params
+        };
 
         // unbox and build optional parameters
         let content_encoding = params.content_encoding;
