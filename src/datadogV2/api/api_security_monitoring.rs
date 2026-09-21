d52de108a886ed31cf7b500f6ed8bb1f187e0bfd
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-21 17:24:42 +0000

    Regenerate client from commit a160e5f of spec repo

--- src/datadogV2/api/api_security_monitoring.rs
+++ src/datadogV2/api/api_security_monitoring.rs
@@ -2280,6 +2280,14 @@ pub enum GetInvestigationLogQueriesMatchingSignalError {
     UnknownValue(serde_json::Value),
 }
 
+/// GetMatchingSignalsError is a struct for typed errors of method [`SecurityMonitoringAPI::get_matching_signals`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum GetMatchingSignalsError {
+    APIErrorResponse(crate::datadogV2::model::APIErrorResponse),
+    UnknownValue(serde_json::Value),
+}
+
 /// GetResourceEvaluationFiltersError is a struct for typed errors of method [`SecurityMonitoringAPI::get_resource_evaluation_filters`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -13942,6 +13950,130 @@ impl SecurityMonitoringAPI {
             };
         } else {
             let local_entity: Option<GetInvestigationLogQueriesMatchingSignalError> =
+                serde_json::from_str(&local_content).ok();
+            let local_error = datadog::ResponseContent {
+                status: local_status,
+                content: local_content,
+                entity: local_entity,
+            };
+            Err(datadog::Error::ResponseError(local_error))
+        }
+    }
+
+    /// Returns the list of security signals that match a given event on the given track.
+    pub async fn get_matching_signals(
+        &self,
+        event_id: String,
+        track: String,
+    ) -> Result<
+        crate::datadogV2::model::MatchingSignalsResponse,
+        datadog::Error<GetMatchingSignalsError>,
+    > {
+        match self
+            .get_matching_signals_with_http_info(event_id, track)
+            .await
+        {
+            Ok(response_content) => {
+                if let Some(e) = response_content.entity {
+                    Ok(e)
+                } else {
+                    Err(datadog::Error::Serde(serde::de::Error::custom(
+                        "response content was None",
+                    )))
+                }
+            }
+            Err(err) => Err(err),
+        }
+    }
+
+    /// Returns the list of security signals that match a given event on the given track.
+    pub async fn get_matching_signals_with_http_info(
+        &self,
+        event_id: String,
+        track: String,
+    ) -> Result<
+        datadog::ResponseContent<crate::datadogV2::model::MatchingSignalsResponse>,
+        datadog::Error<GetMatchingSignalsError>,
+    > {
+        let local_configuration = &self.config;
+        let local_operation_id = "v2.get_matching_signals";
+        if local_configuration.is_unstable_operation_enabled(local_operation_id) {
+            warn!("Using unstable operation {local_operation_id}");
+        } else {
+            let local_error = datadog::UnstableOperationDisabledError {
+                msg: "Operation 'v2.get_matching_signals' is not enabled".to_string(),
+            };
+            return Err(datadog::Error::UnstableOperationDisabledError(local_error));
+        }
+
+        let local_client = &self.client;
+
+        let local_uri_str = format!(
+            "{}/api/v2/security_monitoring/events/{event_id}/matching_signals",
+            local_configuration.get_operation_host(local_operation_id),
+            event_id = datadog::urlencode(event_id)
+        );
+        let mut local_req_builder =
+            local_client.request(reqwest::Method::GET, local_uri_str.as_str());
+
+        local_req_builder = local_req_builder.query(&[("track", &track.to_string())]);
+
+        // build headers
+        let mut headers = HeaderMap::new();
+        headers.insert("Accept", HeaderValue::from_static("application/json"));
+
+        // build user agent
+        match HeaderValue::from_str(local_configuration.user_agent.as_str()) {
+            Ok(user_agent) => headers.insert(reqwest::header::USER_AGENT, user_agent),
+            Err(e) => {
+                log::warn!("Failed to parse user agent header: {e}, falling back to default");
+                headers.insert(
+                    reqwest::header::USER_AGENT,
+                    HeaderValue::from_static(datadog::DEFAULT_USER_AGENT.as_str()),
+                )
+            }
+        };
+
+        // build auth
+        if let Some(local_key) = local_configuration.auth_keys.get("apiKeyAuth") {
+            headers.insert(
+                "DD-API-KEY",
+                HeaderValue::from_str(local_key.key.as_str())
+                    .expect("failed to parse DD-API-KEY header"),
+            );
+        };
+        if let Some(local_key) = local_configuration.auth_keys.get("appKeyAuth") {
+            headers.insert(
+                "DD-APPLICATION-KEY",
+                HeaderValue::from_str(local_key.key.as_str())
+                    .expect("failed to parse DD-APPLICATION-KEY header"),
+            );
+        };
+
+        local_req_builder = local_req_builder.headers(headers);
+        let local_req = local_req_builder.build()?;
+        log::debug!("request content: {:?}", local_req.body());
+        let local_resp = local_client.execute(local_req).await?;
+
+        let local_status = local_resp.status();
+        let local_content = local_resp.text().await?;
+        log::debug!("response content: {}", local_content);
+
+        if !local_status.is_client_error() && !local_status.is_server_error() {
+            match serde_json::from_str::<crate::datadogV2::model::MatchingSignalsResponse>(
+                &local_content,
+            ) {
+                Ok(e) => {
+                    return Ok(datadog::ResponseContent {
+                        status: local_status,
+                        content: local_content,
+                        entity: Some(e),
+                    })
+                }
+                Err(e) => return Err(datadog::Error::Serde(e)),
+            };
+        } else {
+            let local_entity: Option<GetMatchingSignalsError> =
                 serde_json::from_str(&local_content).ok();
             let local_error = datadog::ResponseContent {
                 status: local_status,
